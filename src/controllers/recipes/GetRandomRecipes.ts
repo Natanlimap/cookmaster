a65ea717f033f0565e11bb86cd6520c1f625bf51
natanlimap <natan.pereira@afinz.com.br>
2022-10-19 16:50:41 -0300

    feat: random

--- src/controllers/recipes/GetRandomRecipes.ts
+++ src/controllers/recipes/GetRandomRecipes.ts
@@ -4,13 +4,9 @@ import { shuffle } from "../../helper";
 
 export class GetRandomRecipes{
      async handle(request: Request, response: Response){
-        
-         let {ingredients} = request.params;
-         let recipes = new Recipe();
-         console.log("teste")
-         let allRecipes = recipes.getAllRecipes();
-         
-         return response.json(shuffle(allRecipes).slice(0, 3).map(m=>m.title));
+        let recipes = new Recipe();
+        let allRecipes = recipes.getAllRecipes();
+        return response.json(shuffle(allRecipes).slice(0, 3).map(m=>m.title));
 
         
      }
